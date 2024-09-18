99a2c38db53fc8c98236503692ac21af8364adc3
EmTrett <150582441+EmTrett@users.noreply.github.com>
2024-09-18 15:05:50 +0200

    Basic funksjonalitet av charactersheet.
    Level up funker med kost
    
    Return to camp recharger vail
    
    Vail drink for HP funker, og reroll trekker bare en charge
    
    Roll for weapon funker men ingen inventory grense
    
    Weapon endrer ikke på stats og kan ikke fjernes

---
+++ itemLists.js
@@ -0,0 +1,25 @@
+let weaponsList = [[
+    {weaponName: 'Warped Femur', requirementValue: 0, requirementStat: '', eqiuppedBenefits: '+1 Strength', damage: 1},
+    {weaponName: 'Blood-pricked Rib Bone', requirementValue: 0, requirementStat: '', eqiuppedBenefits: '+1 Dexterity', damage: 1},
+    {weaponName: 'Skullshard Staff', requirementValue: 0, requirementStat: '',eqiuppedBenefits: '+1 Intelligence', damage: 1},
+    {weaponName: 'Bile-Soaked Sack', requirementValue: 0, requirementStat: '',eqiuppedBenefits: '+1 Faith', damage: 1},
+],
+[
+    {weaponName: 'Rotwood Hammer', requirementValue: 1, requirementStat: 'Strength', eqiuppedBenefits: '+1 Strenght', damage: 1},
+    {weaponName: 'Sharpened Bone', requirementValue: 1, requirementStat: 'Dexterity', eqiuppedBenefits: '+1 Dexterity', damage: 1},
+    {weaponName: 'Wand of Fingers', requirementValue: 1, requirementStat: 'Intelligence', eqiuppedBenefits: '+1 Intelligence', damage: 1},
+    {weaponName: 'Pulsating Eyeball', requirementValue: 1, requirementStat: 'faith', eqiuppedBenefits: '+1 Faith', damage: 1},
+],
+[
+    {weaponName: 'Skin-Crested Mace', requirementValue: 2, requirementStat: 'Strength', eqiuppedBenefits: '+1 Strength, -1 Endurance', damage: '2'},
+    {weaponName: 'Jawbone Dagger', requirementValue: 2, requirementStat: 'Dexterity', eqiuppedBenefits: '+1 Dexterity, -1 Faith', damage: '2'},
+    {weaponName: 'Wicked Tongue Staff', requirementValue: 2, requirementStat: 'Intelligence', eqiuppedBenefits: '+1 Intelligence, -1 Strenght', damage: '2'},
+    {weaponName: 'Rotten Scab', requirementValue: 2, requirementStat: 'Faith', eqiuppedBenefits: '+1 Faith, -1 Dexterity', damage: '2'},
+],
+[
+    {weaponName: 'Shoulderblade Shield', requirementValue: 3, requirementStat: 'Strenght & Endurance', eqiuppedBenefits: '+2 Strength, +2 Endurance', damage: 2},
+    {weaponName: 'Giant\'s Fingerbone', requirementValue: 3, requirementStat: 'Dexterity & Faith', eqiuppedBenefits: '+2 Dexterity. Heal 1HP when you deal damage', damage: 2},
+    {weaponName: 'Brainclump Staff', requirementValue: 3, requirementStat: 'Intelligence & Dexterity', eqiuppedBenefits: '+2 Intelligence. If you deal damage to and enemy, they cannot critical hit you on their next attack', damage: 2},
+    {weaponName: 'Sinnew Whip', requirementValue: 3, requirementStat: 'Faith & Intelligence', eqiuppedBenefits: '+2 Faith. You attack once for free at the start of combat', damage: 2},
+]
+];
---
+++ playerInfo.js
@@ -0,0 +1,25 @@
+// Statistics. You have six statistics which form your character:
+// - Vitality. Your health. For every point in Vitality, increase
+// your maximum HP by 2.
+// - Endurance. Your ability to dodge attacks.
+// - Strength. Your ability to use heavy weapons.
+// - Dexterity. Your ability to use nimble weapons.
+// - Intelligence. Your ability to use magical weapons.
+// - Faith. Your ability to use holy weapons
+
+
+
+stats = [
+    {stat: 'vitality', level: 0, cost: 4, displayName: 'Vitality'},
+    {stat: 'endurance', level: 0, cost: 4, displayName: 'Endurance'},
+    {stat: 'strenght', level: 0, cost: 4, displayName: 'Strenght'},
+    {stat: 'dexterity', level: 0, cost: 4, displayName: 'Dexterity'},
+    {stat: 'intelligence', level: 0, cost: 4, displayName: 'Intelligence'},
+    {stat: 'faith', level: 0, cost: 4, displayName: 'Faith'},
+];
+
+currency = [
+    {currencyType: 'boneFragments', amount: 0, displayName: 'Bone Fragments'},
+    {currencyType: 'earthenBulbs', amount: 3, displayName: 'Earthen Bulbs'},
+]
+
